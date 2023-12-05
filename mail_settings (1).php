564ee0defb31a11067625406ec272a7ca0ccdb52
robbie reta <robbie.ret67@gmail.com>
2023-12-04 18:55:41 -0600

    .

--- mail_settings (1).php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$email_to = "robbie.reta67@gmail.com"; // your email address send TO
-$email_from = "contacto@alejandrofonz.com"; // your email address send FROM
-$email_subject = "Contact form message";
-$thankyou = "thankyou.htm";
-
-?>
